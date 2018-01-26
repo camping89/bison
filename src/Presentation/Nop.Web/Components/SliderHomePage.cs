f8e4d605c81bbaba6303b864683f1408ee029f5d
Nguyễn Ngọc Huy <huy.nn@outlook.com>
2018-01-26 13:53:51 +0700

    Processing homepage

---
+++ src/Presentation/Nop.Web/Components/SliderHomePage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+using Nop.Web.Models.Slider;
+
+namespace Nop.Web.Components
+{
+    public class SliderHomePageViewComponent : NopViewComponent
+    {
+       
+        public SliderHomePageViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            List<SliderModel> sliderModels = new List<SliderModel>();
+            return View(sliderModels);
+        }
+    }
+}
