e85570310c1372c66b4eb21c3ceec608220573df
Huy Ngoc Nguyen <romeohuy@gmail.com>
2018-03-08 11:09:35 +0700

    Fix duplicate name category and product when integration data

--- src/Plugins/Nop.Plugin.Integration.KiotViet/Integration/KiotViet/KiotVietApiConsumer.cs
+++ src/Plugins/Nop.Plugin.Integration.KiotViet/Integration/KiotViet/KiotVietApiConsumer.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 using Nop.Core.Extensions;
 using Nop.Plugin.Integration.KiotViet.Integration.KiotViet.Entities;
 using RestSharp;
@@ -9,10 +10,11 @@ namespace Nop.Plugin.Integration.KiotViet.Integration.KiotViet
     public class KiotVietApiConsumer
     {
         protected RestClient Client;
-
+        private string _token;
         public KiotVietApiConsumer()
         {
-            Client = new RestClient("https://public.kiotapi.com");
+            Client = new RestClient(KiotVietConstant.UrlApiRootPublic);
+            _token = GetToken();
         }
 
         private ApiResponse<T> HandleResponse<T>(IRestResponse response) where T : new()
@@ -35,8 +37,8 @@ namespace Nop.Plugin.Integration.KiotViet.Integration.KiotViet
 
         public string GetToken()
         {
-            var tokenClient = new RestClient("https://id.kiotviet.vn/");
-            var request = new RestRequest("connect/token", Method.POST);
+            var tokenClient = new RestClient(KiotVietConstant.UrlApiRootToken);
+            var request = new RestRequest(KiotVietConstant.UrlApiGetToken, Method.POST);
             request.AddParameter("client_id", "8a80ab27-b951-4ad1-879b-c898065f0c1d");
             request.AddParameter("client_secret", "F3462C9643299B121CD4486DCAC620CDDCC03122");
             request.AddParameter("grant_type", "client_credentials");
@@ -47,5 +49,43 @@ namespace Nop.Plugin.Integration.KiotViet.Integration.KiotViet
 
             return apiResponse.Resource?.access_token ?? string.Empty;
         }
+
+        public List<KVCategory> GetKVCategories()
+        {
+            if (string.IsNullOrEmpty(_token))
+            {
+                return null;
+            }
+            var request = new RestRequest(KiotVietConstant.UrlApiGetCategory, Method.GET);
+            request.AddHeader("Retailer", "bisonkiotvietcom");
+            request.AddHeader("Authorization", $"Bearer {_token}");
+            var response = Client.Execute(request);
+            var apiResponse = HandleResponse<GetCategoryResponse>(response);
+            if (apiResponse.Status == HttpStatusCode.Unauthorized)
+            {
+                _token = GetToken();
+            }
+            return apiResponse.Resource.data;
+        }
+
+        public List<KVProduct> GetProductsByCategoryId(int categoryId)
+        {
+            if (string.IsNullOrEmpty(_token))
+            {
+                return null;
+            }
+            var request = new RestRequest(string.Format(KiotVietConstant.UrlApiGetProduct,categoryId), Method.GET);
+            request.AddHeader("Retailer", "bisonkiotvietcom");
+            request.AddHeader("Authorization", $"Bearer {_token}");
+            var response = Client.Execute(request);
+            var apiResponse = HandleResponse<GetProductResponse>(response);
+            if (apiResponse.Status == HttpStatusCode.Unauthorized)
+            {
+                _token = GetToken();
+            }
+            return apiResponse.Resource.data;
+        }
+
+
     }
 }
--- src/Plugins/Nop.Plugin.Integration.KiotViet/Integration/ScheduleTasks/SyncProductTask.cs
+++ src/Plugins/Nop.Plugin.Integration.KiotViet/Integration/ScheduleTasks/SyncProductTask.cs
@@ -1,23 +1,126 @@
-﻿using Nop.Core.Domain.Logging;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Nop.Core.Domain.Logging;
 using Nop.Plugin.Integration.KiotViet.Integration.KiotViet;
 using Nop.Services.Logging;
 using Nop.Services.Tasks;
+using Newtonsoft.Json;
+using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Media;
+using Nop.Core.Extensions;
+using Nop.Plugin.Integration.KiotViet.Integration.KiotViet.Entities;
+using Nop.Services.Catalog;
+using Nop.Services.Media;
+using Nop.Services.Seo;
 
 namespace Nop.Plugin.Integration.KiotViet.Integration.ScheduleTasks
 {
     public class SyncProductTask : IScheduleTask
     {
         private KiotVietApiConsumer _apiConsumer;
+        private KiotVietService _kiotVietService;
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+        private readonly IUrlRecordService _urlRecordService;
+        private readonly IPictureService _pictureService;
         private ILogger _logger;
-        public SyncProductTask(ILogger logger)
+        public SyncProductTask(ILogger logger, ICategoryService categoryService, 
+            IProductService productService, IUrlRecordService urlRecordService, IPictureService pictureService)
         {
             _logger = logger;
+            _categoryService = categoryService;
+            _productService = productService;
+            _urlRecordService = urlRecordService;
+            _pictureService = pictureService;
             _apiConsumer = new KiotVietApiConsumer();
+            _kiotVietService = new KiotVietService(categoryService,urlRecordService);
         }
 
         public void Execute()
         {
-            _logger.InsertLog(LogLevel.Information, _apiConsumer.GetToken());
+            _logger.InsertLog(LogLevel.Information, "Sync data KiotViet to Bison nopcommerce.");
+            //Sync Catalog 
+            _kiotVietService.InsertKiotVietCatalog(_apiConsumer.GetKVCategories());
+            var categories = _categoryService.GetAllCategories(showHidden: true);
+            foreach (var cate in categories)
+            {
+                var lstProducts = _apiConsumer.GetProductsByCategoryId(cate.KiotVietCateId);
+                foreach (var kvProduct in lstProducts)
+                {
+                    var checkProc = _productService.GetProductBySku(kvProduct.code);
+                    if (checkProc == null)
+                    {
+                        var nopProduct = ConvertKVProcToNopProduct(kvProduct);
+                        _productService.InsertProduct(nopProduct);
+                        if (nopProduct.Id > 0)
+                        {
+                            string seName = nopProduct.ValidateSeName(string.Empty, nopProduct.Name, true);
+                            _urlRecordService.SaveSlug(nopProduct, seName, 0);
+                            SaveCategoryMappings(nopProduct.Id, kvProduct.categoryId);
+                            if (kvProduct.images != null)
+                            {
+                                foreach (var img in kvProduct.images)
+                                {
+                                    InsertProductPictureFromUrl(img, seName, nopProduct.Id);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void InsertProductPictureFromUrl(string urlImage,string seName,int productId)
+        {
+            var webClient = new WebClient();
+            byte[] imageBytes = webClient.DownloadData(urlImage);
+            var picture = _pictureService.InsertPicture(imageBytes, "image/jpeg", seName, validateBinary: false);
+            _productService.InsertProductPicture(new ProductPicture
+            {
+                PictureId = picture.Id,
+                ProductId = productId,
+                DisplayOrder = 0,
+            });
+        }
+        private void SaveCategoryMappings(int productId,int kvCategoryId)
+        {
+            var cate = _categoryService.GetCategoryByKVCateId(kvCategoryId);
+            if (cate != null)
+            {
+                var displayOrder = 1;
+                _categoryService.InsertProductCategory(new ProductCategory
+                {
+                    ProductId = productId,
+                    CategoryId = cate.Id,
+                    DisplayOrder = displayOrder
+                });
+            }
+        }
+        private Product ConvertKVProcToNopProduct(KVProduct kvProduct)
+        {
+            return new Product
+            {
+                ProductTypeId = ProductType.SimpleProduct.ToInt(),
+                VisibleIndividually = true,
+                Name = kvProduct.name,
+                ShortDescription = kvProduct.fullName,
+                FullDescription = kvProduct.description,
+                ProductTemplateId = 1, //1: Simple product  | 2: Grouped product(with variants)
+                AllowCustomerReviews = true,
+                Sku = kvProduct.code,
+                DisplayStockAvailability = true,
+                DisplayStockQuantity = false,
+                LowStockActivityId = 1,
+                NotifyAdminForQuantityBelow = 1,
+                OrderMinimumQuantity = 1,
+                OrderMaximumQuantity = 10000,
+                Price = kvProduct.basePrice,
+                CreatedOnUtc = DateTime.UtcNow,
+                UpdatedOnUtc = DateTime.UtcNow,
+                Published = true
+            };
         }
     }
 }
